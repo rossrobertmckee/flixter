8d8afa5fc17fee109a2b59c6fd9bffe9c1f8fa08
Ross McKee <rossrobertmckee@gmail.com>
2015-06-23 22:48:00 +0000

    Video link

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-gem 'fog', '~> 1.3.1'
+gem 'carrierwave-aws'
 
 gem 'figaro'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622013413) do
+ActiveRecord::Schema.define(version: 20150623223957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150622013413) do
     t.integer  "section_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "video"
   end
 
   add_index "lessons", ["section_id"], name: "index_lessons_on_section_id", using: :btree
