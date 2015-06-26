5d1cd47b16ca83d7c6c6f1a1419ac71e007898a9
Ross Robert McKee <rossrobertmckee@Rosss-MacBook-Air.local>
2015-06-26 18:25:55 -0400

    Login and Signup Modals

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,17 @@
 class StaticPagesController < ApplicationController
 	def index
 	end
+
+
+  	def resource_name
+  	:user
+	end
+
+	def resource
+  	@resource ||= User.new
+	end
+
+	def devise_mapping
+ 		@devise_mapping ||= Devise.mappings[:user]
+	end
 end
