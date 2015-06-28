d8d7dee07b2ff3099d243091fb33c1667ada8d28
Ross Robert McKee <rossrobertmckee@gmail.com>
2015-06-28 13:19:19 -0400

    More styling

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,7 +26,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [800, 350]
+  process :resize_to_fill => [600, 600]
   #
   # def scale(width, height)
   #   # do something
